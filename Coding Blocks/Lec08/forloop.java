8c91f78ec6567346ca4b9ded4f316c0d4ed2de84
Gourav <133738784+bob0builder@users.noreply.github.com>
2026-01-29 00:37:45 +0530

    another day

---
+++ Coding Blocks/Lec08/forloop.java
@@ -0,0 +1,19 @@
+public class forloop {
+    public static void main(String[] args) {
+        int i = 1;
+        for(i = 1; i<= 10; i++){
+            System.out.println(i);
+        }
+
+        int j = 1;
+        for(;j<=10;){
+            System.out.println(j);
+            j++;
+        }
+        for(;j<=5; j++){
+            System.out.println(j);
+
+        }
+
+    }
+}
--- Coding Blocks/Lec08/pattern33.java
+++ Coding Blocks/Lec08/pattern33.java
@@ -1,33 +1,35 @@
 public class pattern33 {
-    public static void main(String[] args){
-        int n = 10;
-        int row = 0;
-        int space = n-1;
-        int val = n-1;
-        int star = 1;
-        while(row<n){
-
-            //printing SPACE
-            int i = 0;
-            while(i<space){
-                System.out.print(" ");
-                i++;
-            }
-            //Star
-            int j = 0;
-            while(j<star){
-                if(val==n) System.out.print("0");
-                else System.out.print(val);
-                if(j<row) val++;
-                else val--;
-
-                j++;
-            }
-            //nextLine
-            row++;
-            space--;
-            star +=2;
-            System.out.println();
+    public static void main(String[] args) {
+        int n =10;
+        int rows =1;
+        int space =n-1;
+        int star =1;
+        int val =n;
+        while(rows<=n){
+            int i=0;
+            while(i<=space){
+            System.out.print("  ");
+            i++;
         }
+        int j =1;
+        while(j<=star){
+            if(val==n){
+                System.out.print("* ");
+            }else{
+                System.out.print(val+" ");
+            }
+            if(j<rows){
+                val++;
+            }else{
+                val--;
+            }
+            j++;
+        }
+        rows++;
+        space--;
+        star+=2;
+        System.out.println();
+    }
+
     }
 }
